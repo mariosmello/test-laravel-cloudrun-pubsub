e23e3ac3d6c8ba3c0cb501a3e2cc776cd218caf0
Mário Mello <marioesmello@gmail.coim>
2024-03-29 17:54:02 -0300

    Test pubsub

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     \App\Jobs\EchoOutput::dispatch(new DateTime());
+    \Illuminate\Support\Facades\Log::info('Before Welcome');
     return view('welcome');
 });
