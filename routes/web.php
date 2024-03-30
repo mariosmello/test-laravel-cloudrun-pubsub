dd1813c747be41f23132e689a05f2c585bae2a69
Mário Mello <marioesmello@gmail.coim>
2024-03-30 11:20:58 -0300

    Test pubsub

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     \App\Jobs\EchoOutput::dispatch(new DateTime());
-    \Illuminate\Support\Facades\Log::error('Before Welcome');
+    \Illuminate\Support\Facades\Log::error('Start Queue');
     return view('welcome');
 });
 
 Route::any('/jobs/', function(\Illuminate\Http\Request $request) {
-    \Illuminate\Support\Facades\Log::error('Jobs', ['request' => $request->all()]);
+    \Illuminate\Support\Facades\Log::error('Start Job');
     $job = new \App\Jobs\EchoOutput($request);
     $job->handle();
-    return response()->json([])->status(200);
+    return response()->json();
 });
